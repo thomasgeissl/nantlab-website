cd915ee63534ebfac224ffd5ebae6a7f80887e16
Thomas Geissl <thomas.geissl@gmail.com>
2021-07-31 19:46:04 +0200

    fixed footer padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { format } from "date-fns";
 const Container = styled.div`
   text-align: right;
+  padding-bottom: 5px;
+  padding-top: 5px;
 `;
 
 export default function Footer() {
